cf3ba9671a72bf2125b486d3608fbd359d996eb3
Alexander Martinz <eviscerationls@gmail.com>
2017-08-15 17:53:55 +0200

    drivers: media: camera import
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -17,21 +17,6 @@
 #include "camera.h"
 #include "msm_cci.h"
 #include "msm_camera_dt_util.h"
-
-/* chenneng@camera 20151117 add for product information */
-#include <linux/project_info.h>
-struct camera_vendor_match_tbl {
-    char sensor_name[32];
-    char vendor_name[32];
-};
-static struct camera_vendor_match_tbl match_tbl[] = {
-    {"imx398","Sony"},
-    {"imx362","Sony"},
-    {"imx350","Sony"},
-    {"imx371","Sony"},
-    {"imx179","Sony"},
-    {"s5k3p8sp","SAMSUNG"},
-};
 
 /* Logging macro */
 #undef CDBG
@@ -478,17 +463,11 @@ static int32_t msm_sensor_create_pd_settings(void *setting,
 
 #ifdef CONFIG_COMPAT
 	if (is_compat_task()) {
-		int i = 0;
-		struct msm_sensor_power_setting32 *power_setting_iter =
-		(struct msm_sensor_power_setting32 *)compat_ptr((
-		(struct msm_camera_sensor_slave_info32 *)setting)->
-		power_setting_array.power_setting);
-
-		for (i = 0; i < size_down; i++) {
-			pd[i].config_val = power_setting_iter[i].config_val;
-			pd[i].delay = power_setting_iter[i].delay;
-			pd[i].seq_type = power_setting_iter[i].seq_type;
-			pd[i].seq_val = power_setting_iter[i].seq_val;
+		rc = msm_sensor_get_pw_settings_compat(
+			pd, pu, size_down);
+		if (rc < 0) {
+			pr_err("failed");
+			return -EFAULT;
 		}
 	} else
 #endif
@@ -718,9 +697,6 @@ int32_t msm_sensor_driver_probe(void *setting,
 	struct msm_camera_cci_client        *cci_client = NULL;
 	struct msm_camera_sensor_slave_info *slave_info = NULL;
 	struct msm_camera_slave_info        *camera_info = NULL;
-/* chenneng@camera 20151117 add for product information */
-	uint32_t count = 0,i;
-	enum COMPONENT_TYPE CameraID;
 
 	unsigned long                        mount_pos = 0;
 	uint32_t                             is_yuv;
@@ -1039,26 +1015,6 @@ CSID_TG:
 
 	/*Save sensor info*/
 	s_ctrl->sensordata->cam_slave_info = slave_info;
-
-/* chenneng@camera 20151117 add for product information */
-    if (0 == slave_info->camera_id)
-        CameraID = R_CAMERA;
-    else if (1 == slave_info->camera_id)
-        CameraID = SECOND_R_CAMERA;
-    else if (2 == slave_info->camera_id)
-        CameraID = F_CAMERA;
-
-    count = ARRAY_SIZE(match_tbl);
-    for (i = 0;i < count;i++) {
-        if (!strcmp(slave_info->sensor_name,match_tbl[i].sensor_name))
-            break;
-    }
-    if (i >= count)
-        pr_err("%s,Match camera sensor faild!,current sensor name is %s",
-            __func__,slave_info->sensor_name);
-    else
-        push_component_info(CameraID,slave_info->sensor_name,
-            match_tbl[i].vendor_name);
 
 	msm_sensor_fill_sensor_info(s_ctrl, probed_info, entity_name);
 
@@ -1379,18 +1335,20 @@ static int32_t msm_sensor_driver_i2c_probe(struct i2c_client *client,
 	if (s_ctrl->sensor_i2c_client != NULL) {
 		s_ctrl->sensor_i2c_client->client = client;
 		s_ctrl->sensordata->power_info.dev = &client->dev;
+
+		/* Get clocks information */
+		rc = msm_camera_i2c_dev_get_clk_info(
+			&s_ctrl->sensor_i2c_client->client->dev,
+			&s_ctrl->sensordata->power_info.clk_info,
+			&s_ctrl->sensordata->power_info.clk_ptr,
+			&s_ctrl->sensordata->power_info.clk_info_size);
+		if (rc < 0) {
+			pr_err("failed: msm_camera_i2c_dev_get_clk_info rc %d",
+				rc);
+			goto FREE_S_CTRL;
+		}
+		return rc;
 	}
-	/* Get clocks information */
-	rc = msm_camera_i2c_dev_get_clk_info(
-		&s_ctrl->sensor_i2c_client->client->dev,
-		&s_ctrl->sensordata->power_info.clk_info,
-		&s_ctrl->sensordata->power_info.clk_ptr,
-		&s_ctrl->sensordata->power_info.clk_info_size);
-	if (rc < 0) {
-		pr_err("failed: msm_camera_i2c_dev_get_clk_info rc %d", rc);
-		goto FREE_S_CTRL;
-	}
-	return rc;
 FREE_S_CTRL:
 	kfree(s_ctrl);
 	return rc;
